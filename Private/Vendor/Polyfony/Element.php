10d2be0da15fc19b567ca85c1aa21c43658ec6f1
Julien <e10ad5d4ab72523920e7cbe55ba6c91c@gribouille.eu>
2015-01-11 22:15:01 +0100

    HTML\Element draft

---
+++ Private/Vendor/Polyfony/Element.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * PHP Version 5
+ * Class to generate Elements
+ * Loosely based on Mootools Element class
+ * @package Polyfony
+ * @link https://github.com/SIB-FRANCE/Polyfony
+ * @license http://www.gnu.org/licenses/lgpl.txt GNU General Public License
+ * @note This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Polyfony;
+ 
+class Element {
+
+	private $type;
+	private $attributes;
+	private $content;
+
+	public function __construct($type='div', $options=array()) {
+		$this->type 		= $type;
+		$this->attributes 	= $options;
+		$this->content 		= '';
+	}
+
+	public function setText($text) {
+		$this->content = Format::htmlSafe($text);
+	}
+
+	public function appendText($text) {
+		$this->content .= Format::htmlSafe($text);
+	}
+
+	public function setHTML($html) {
+		$this->content = $html;
+	}
+
+	public function set($attribute, $value) {
+		$this->attributes[$attribute] = $value;
+	}
+
+	public function get($attribute) {
+		return(isset($this->attributes[$attribute] ? $this->attributes[$attribute] : null));
+	}
+
+	public function adopt(Element $child) {
+		$this->content .= $child;
+	}
+
+	public function __toString() {
+		// magic goes here
+	}
+
+
+}
+
+?>
