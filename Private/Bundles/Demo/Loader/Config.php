d25572fde75e2ca74447b63c4a716a78c6a37202
Julien <nospam.github@gribouille.eu>
2018-02-12 15:00:07 +0100

    BROKEN VERSION
    Removing all varialbe data from versionning.

--- Private/Bundles/Demo/Loader/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Polyfony as pf;
-
-// set some demo data
-pf\Config::set('demo', 'some_key', 'some_value');
-
-?>
--- Private/Bundles/Demo/Views/Index.php
+++
@@ -1,9 +0,0 @@
-<div class="jumbotron" style="padding:45px;">
-	<h1>Polyfony</h1>
-	<p>Polyfony is a simple and powerful PHP micro-framework</p>
-	<p>
-		<a class="btn btn-success btn-lg" href="http://github.com/sib-france/polyfony" role="button">Documentation on GitHub</a> 
-		<a class="btn btn-warning btn-lg" href="/tools/" role="button"/>Tools</a> 
-		<a class="btn btn-primary btn-lg" href="/demo/" role="button"/>Demo</a>
-	</p>
-</div>
