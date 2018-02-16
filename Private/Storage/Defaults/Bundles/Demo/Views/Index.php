1c8a4165426a5cdc222240846852abbf45c20b8e
Julien <nospam.github@gribouille.eu>
2018-02-16 15:53:21 +0100

    Aesthetics on GET:/

--- Private/Storage/Defaults/Bundles/Demo/Views/Index.php
+++ Private/Storage/Defaults/Bundles/Demo/Views/Index.php
@@ -2,7 +2,7 @@
 	<h1>Polyfony</h1>
 	<p>Polyfony is a simple and powerful PHP micro-framework</p>
 	<p>
-		<a class="btn btn-success btn-lg" href="http://github.com/polyfony-inc/polyfony" role="button">Documentation on GitHub</a> 
-		<a class="btn btn-primary btn-lg" href="/demo/" role="button"/>Demo</a>
+		<a class="btn btn-primary btn-lg" href="http://github.com/polyfony-inc/polyfony" role="button">Documentation on GitHub</a> 
+		<a class="btn btn-secondary btn-lg" href="/demo/" role="button"/>Demo</a>
 	</p>
 </div>
